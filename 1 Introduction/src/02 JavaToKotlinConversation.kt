2cb0110fccd52e1b8629fecf464a913b1dac46e5
Elena Listratova <listratova.elena.sergeevna@gmail.com>
2017-08-13 07:15:09 +0400

    kotlin lab 4

--- 1 Introduction/src/02 JavaToKotlinConversation.kt
+++ 1 Introduction/src/02 JavaToKotlinConversation.kt
@@ -1,16 +1,14 @@
-class JavaCode {
-    fun toJSON(collection: Collection<Int>): String {
-        val sb = StringBuilder()
-        sb.append("[")
-        val iterator = collection.iterator()
-        while (iterator.hasNext()) {
-            val element = iterator.next()
-            sb.append(element)
-            if (iterator.hasNext()) {
-                sb.append(", ")
-            }
+fun toJSON(collection: Collection<Int>): String {
+    val sb = StringBuilder()
+    sb.append("[")
+    val iterator = collection.iterator()
+    while (iterator.hasNext()) {
+        val element = iterator.next()
+        sb.append(element)
+        if (iterator.hasNext()) {
+            sb.append(", ")
         }
-        sb.append("]")
-        return sb.toString()
     }
+    sb.append("]")
+    return sb.toString()
 }
