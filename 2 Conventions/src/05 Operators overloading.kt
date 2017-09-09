22f4ba8087fcf87f78ead839135bf638139b8f51
Elena Listratova <listratova.elena.sergeevna@gmail.com>
2017-09-09 10:42:53 +0400

    2-5

--- 2 Conventions/src/05 Operators overloading.kt
+++ 2 Conventions/src/05 Operators overloading.kt
@@ -9,4 +9,21 @@
  * 
  * Then, try to support adding several time intervals to a date. You may need an extra class.
  */
- 
+import TimeInterval.*
+
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+
+enum class TimeInterval { DAY, WEEK, YEAR }
+
+operator fun MyDate.plus(timeInterval: TimeInterval): MyDate = addTimeIntervals(timeInterval, 1)
+
+fun task1(today: MyDate): MyDate {
+    return today + YEAR + WEEK
+}
+
+fun task2(today: MyDate): MyDate {
+    return today 
+    .addTimeIntervals(TimeInterval.YEAR, 2) 
+    .addTimeIntervals(TimeInterval.WEEK, 3) 
+    .addTimeIntervals(TimeInterval.DAY, 5)
+}
