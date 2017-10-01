55bffebbdadd7ed36aaceb612c063757593bf06c
Elena Listratova <listratova.elena.sergeevna@gmail.com>
2017-10-01 20:41:19 +0400

    2-6

---
+++ 2 Conventions/src/06 Destructuring declarations.kt
@@ -0,0 +1,15 @@
+/**
+ * http://kotlinlang.org/docs/reference/multi-declarations.html
+ * https://kotlinlang.ru/docs/reference/multi-declarations.html
+ *
+ * Read about destructuring declarations and make the following code compile by adding one word.
+ */
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+
+fun isLeapDay(date: MyDate): Boolean {
+
+    val (year, month, dayOfMonth) = date
+
+    // 29 February of a leap year
+    return year % 4 == 0 && month == 2 && dayOfMonth == 29
+}
