6b46a9b31c9ece86d23d6ca0034772fbecc07c0e
Elena Listratova <listratova.elena.sergeevna@gmail.com>
2017-10-04 21:51:45 +0400

    2-7

---
+++ 2 Conventions/src/07 Invoke.kt
@@ -0,0 +1,20 @@
+/**
+ * Objects with invoke() method can be invoked as a function.
+ * 
+ * You can add invoke extension for any class, but it's better not to overuse it:
+ * 
+ * operator fun Int.invoke() { println(this) }
+ * 1() //huh?..
+ * 
+ * Implement the function Invokable.invoke() so it would count a number of invocations.
+ */
+class Invokable {
+    var numberOfInvocations: Int = 0
+        private set
+    operator fun invoke(): Invokable {
+        numberOfInvocations++
+        return this
+    }
+}
+
+fun invokeTwice(invokable: Invokable) = invokable()()
