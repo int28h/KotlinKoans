e03f30f6446c371abeb13059b1d8cf3e77573b33
Elena Listratova <listratova.elena.sergeevna@gmail.com>
2017-10-18 20:48:07 +0400

    3-6 task

---
+++ 3 Collections/06 Sort.kt
@@ -0,0 +1,13 @@
+/**
+ * Implement Shop.getCustomersSortedByNumberOfOrders() using sorted or sortedBy.
+ * 
+ * listOf("bbb", "a", "cc").sorted() == listOf("a", "bbb", "cc")
+ * listOf("bbb", "a", "cc").sortedBy { it.length } == listOf("a", "cc", "bbb")
+ * 
+ * data class Shop(val name: String, val customers: List<Customer>)
+ * data class Customer(val name: String, val city: City, val orders: List<Order>)
+ *
+ * https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/kotlin.-iterable/sorted.html
+ * https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/kotlin.-iterable/sorted-by.html
+ */
+
