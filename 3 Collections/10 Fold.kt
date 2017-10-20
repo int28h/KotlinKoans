41e6c14a94ca9c2d0877b933e0baa4c98f6f78da
Elena Listratova <listratova.elena.sergeevna@gmail.com>
2017-10-20 22:10:20 +0400

    3-10 task

---
+++ 3 Collections/10 Fold.kt
@@ -0,0 +1,14 @@
+/**
+ * Implement Shop.getSetOfProductsOrderedByEveryCustomer() using fold.
+ * 
+ * listOf(1, 2, 3, 4).fold(1, {
+ *     partProduct, element ->
+ *     element * partProduct
+ *  * }) == 24
+
+ * data class Shop(val name: String, val customers: List<Customer>)
+ * data class Customer(val name: String, val city: City, val orders: List<Order>)
+ * data class Order(val products: List<Product>, val isDelivered: Boolean)
+ * 
+ * https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/kotlin.-iterable/fold.html
+ */
