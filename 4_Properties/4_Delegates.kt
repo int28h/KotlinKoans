04cb1c49a9a2a9a7cfce9bebe3b8316a7328d093
Elena Listratova <listratova.elena.sergeevna@gmail.com>
2018-03-28 06:15:34 +0400

    delegates 2

---
+++ 4_Properties/4_Delegates.kt
@@ -0,0 +1,27 @@
+/*
+You may declare your own delegates. Implement the methods of the class 'EffectiveDate' 
+so it can be delegated to. Store only the time in milliseconds in 'timeInMillis' property.
+
+Use the extension functions MyDate.toMillis() and Long.toDate().
+
+https://kotlinlang.org/docs/reference/delegated-properties.html#property-delegate-requirements
+*/
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+class D {
+    var date: MyDate by EffectiveDate()
+}
+
+class EffectiveDate<R> : ReadWriteProperty<R, MyDate> {
+
+    var timeInMillis: Long? = null
+
+    override fun getValue(thisRef: R, property: KProperty<*>): MyDate {
+        return timeInMillis!!.toDate()
+    }
+
+    override fun setValue(thisRef: R, property: KProperty<*>, value: MyDate) {
+        timeInMillis = value.toMillis()
+    }
+}
