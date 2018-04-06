fffcdcbb909efa42f714c4c7194c985062b9a40e
Elena Listratova <listratova.elena.sergeevna@gmail.com>
2018-04-06 04:51:48 +0400

    empty file

---
+++ 3_Collections/12_Get_used_to_new_style.kt
@@ -0,0 +1,31 @@
+/*
+Rewrite the following Java function to Kotlin.
+
+public Collection<String> doSomethingStrangeWithCollection(
+        Collection<String> collection
+) {
+    Map<Integer, List<String>> groupsByLength = Maps.newHashMap();
+    for (String s : collection) {
+        List<String> strings = groupsByLength.get(s.length());
+        if (strings == null) {
+            strings = Lists.newArrayList();
+            groupsByLength.put(s.length(), strings);
+        }
+        strings.add(s);
+    }
+
+    int maximumSizeOfGroup = 0;
+    for (List<String> group : groupsByLength.values()) {
+        if (group.size() > maximumSizeOfGroup) {
+            maximumSizeOfGroup = group.size();
+        }
+    }
+
+    for (List<String> group : groupsByLength.values()) {
+        if (group.size() == maximumSizeOfGroup) {
+            return group;
+        }
+    }
+    return null;
+}
+*/
